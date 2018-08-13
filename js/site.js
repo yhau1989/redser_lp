580cc4e6d90e242ff3c8083028ffe14c95dea7d5
DEPRATI\spilay <samuel.pilay@deprati.com.ec>
2018-08-13 12:25:43 -0500

    footer

--- js/site.js
+++ js/site.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-  
-    $('#myMenuSlide').click(function (e) {
+$(document).ready(function() {
+
+    $('#myMenuSlide').click(function(e) {
 
         $('.ui.sidebar')
-             .sidebar('toggle');
+            .sidebar('toggle');
 
     });
 
@@ -13,12 +13,11 @@ function focusPath() {
     var pathArray = window.location.pathname.split("/");
     var path = "";
 
-    if (pathArray.length > 0)
-    {
+    if (pathArray.length > 0) {
         path = pathArray[pathArray.length - 1].replace(".html", "");
         var p = document.querySelectorAll('#menuroot a');
         p.forEach(key => getHref(key, path));
-        
+
     }
 }
 
@@ -27,36 +26,63 @@ function getHref(key, newPathname) {
     key.classList.remove('linkactive');
     const link = key.getAttribute("href");
     if (!link) return;
-    if (link.replace(".html", "").replace("./","").toLowerCase() == newPathname.toLowerCase()) {
+    if (link.replace(".html", "").replace("./", "").toLowerCase() == newPathname.toLowerCase()) {
         key.classList.add('active');
         key.classList.add('linkactive');
     }
 }
 
 
-function resiz(media)
-{
-    if (media.matches)
-    {
+function resiz(media) {
+    if (media.matches) {
         //document.body.style.background = 'red';
         var footer = document.getElementById("foo");
-        console.log(footer);
-        footer.classList.remove("myfooter2");
-        footer.classList.add("myfooter");
-    }
-    else
-    {
+
+        if (footer) {
+            footer.classList.remove("myfooter2");
+            footer.classList.add("myfooter");
+        }
+
+    } else {
         //document.body.style.background = 'yellow';
         var footer = document.getElementById("foo");
-        footer.classList.remove("myfooter");
-        footer.classList.add("myfooter2");
+        if (footer) {
+            footer.classList.remove("myfooter");
+            footer.classList.add("myfooter2");
+        }
+
+
+    }
+}
+
+function resizeIndex(media) {
+    if (media.matches) {
+        //document.body.style.background = 'red';
+        var fooidenx = document.getElementById("fooidenx");
+        if (fooidenx) {
+
+            fooidenx.classList.remove("myfooter2");
+            fooidenx.classList.add("myfooter");
+        }
+
+    } else {
+        //document.body.style.background = 'yellow';
+        var fooidenx = document.getElementById("fooidenx");
+        if (fooidenx) {
+            fooidenx.classList.remove("myfooter");
+            fooidenx.classList.add("myfooter2");
+        }
     }
 }
 
 
-var media = window.matchMedia('(min-width: 767px)')
+var media = window.matchMedia('(min-width: 720px)')
+var mediaIndex = window.matchMedia('(max-width: 720px)')
 
 media.addListener(resiz);
 resiz(media);
+
+mediaIndex.addListener(resizeIndex);
+resizeIndex(mediaIndex);
 
 window.addEventListener('onload', focusPath());
