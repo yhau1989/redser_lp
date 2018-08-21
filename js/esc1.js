19ca1140cd5581137b148645a9cd15b73ad7541b
DEPRATI\spilay <samuel.pilay@deprati.com.ec>
2018-08-21 17:13:35 -0500

    funcionalidad

---
+++ js/esc1.js
@@ -0,0 +1,73 @@
+const endpoint_intereses = 'https://resdec-solution-web.herokuapp.com/resdec/list_interests/?var_environment_id=1';
+const endpoint_tags = 'https://resdec-solution-web.herokuapp.com/resdec/list_features/?relationship_type_id=1&var_environment_id=1&feature_name=';
+
+
+$(document).ready(function() {
+
+    //lenar combo de intereses
+    $.getJSON(endpoint_intereses,
+        function(data) {
+            if (data.list_interests) {
+                var json = data.list_interests;
+                var x = document.getElementById("intereses");
+
+                for (var clave in json) {
+                    // Controlando que json realmente tenga esa propiedad
+                    if (json.hasOwnProperty(clave)) {
+                        // Mostrando en pantalla la clave junto a su valor
+                        console.log("La clave es " + clave + " y el valor es " + json[clave]);
+                        var option = document.createElement("option");
+                        option.value = clave;
+                        option.text = json[clave];
+                        x.add(option);
+                    }
+                }
+            }
+        });
+
+    //lenar combo de tags
+    $.getJSON(endpoint_tags,
+        function(data) {
+            if (data.list_features) {
+                var json = data.list_features;
+                var x = document.getElementById("seach_tags");
+
+                for (var clave in json) {
+                    // Controlando que json realmente tenga esa propiedad
+                    if (json.hasOwnProperty(clave)) {
+                        // Mostrando en pantalla la clave junto a su valor
+                        //console.log("La clave es " + clave + " y el valor es " + json[clave]);
+                        var option = document.createElement("option");
+                        option.value = clave;
+                        option.text = json[clave];
+                        x.add(option);
+                    }
+                }
+            }
+        });
+
+
+
+
+});
+
+
+
+
+
+function process() {
+
+    console.log('process');
+    var select_a = document.querySelectorAll('div.ui.fluid.search.dropdown.selection.multiple a');
+    if (select_a) {
+        select_a.forEach(function(x) {
+                console.log(x.getAttribute("data-value"));
+            })
+            //console.log(select_a);
+    }
+
+
+}
+
+const btSummit = document.getElementById('sub'); //.addEventListener("click", process);
+btSummit.addEventListener("click", process);
