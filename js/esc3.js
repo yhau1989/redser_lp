eb40a0f61154633917b01fd559f2d034ec2580b6
DEPRATI\spilay <samuel.pilay@deprati.com.ec>
2018-08-27 11:23:38 -0500

    Cambios con en ambiente 2 y 3 y nueva imagen del logo con fondo blanco

---
+++ js/esc3.js
@@ -0,0 +1,83 @@
+const endpoint_tags = 'https://resdec-solution-web.herokuapp.com/resdec/list_features/?relationship_type_id=1&var_environment_id=1&feature_name=';
+
+
+$(document).ready(function() {
+
+
+
+    //lenar combo de tags
+    $.getJSON(endpoint_tags,
+        function(data) {
+            if (data.list_features) {
+                var json = data.list_features;
+                var x = document.getElementById("seach_tags");
+
+                for (var clave in json) {
+                    // Controlando que json realmente tenga esa propiedad
+                    if (json.hasOwnProperty(clave)) {
+                        // Mostrando en pantalla la clave junto a su valor
+                        //console.log("La clave es " + clave + " y el valor es " + json[clave]);
+                        var option = document.createElement("option");
+                        option.value = clave;
+                        option.text = json[clave];
+                        x.add(option);
+                    }
+                }
+            }
+        });
+
+
+
+
+});
+
+
+
+
+
+function process() {
+
+    console.log('process');
+    var select_a = document.querySelectorAll('div.ui.fluid.search.dropdown.selection.multiple a');
+    if (select_a) {
+        select_a.forEach(function(x) {
+                console.log(x.getAttribute("data-value"));
+            })
+            //console.log(select_a);
+    }
+}
+
+function add() {
+    var txtb = document.getElementById('numbers_suggestions');
+    if (txtb) {
+        if (txtb.value.length > 0) {
+            var x = txtb.value;
+            txtb.value = (Number(x) + 1);
+        } else {
+            txtb.value = 1;
+        }
+    }
+
+}
+
+function minus() {
+    var txtb = document.getElementById('numbers_suggestions');
+    if (txtb) {
+        if (txtb.value.length > 0 && Number(txtb.value) > 1) {
+            var x = txtb.value;
+            txtb.value = (Number(x) - 1);
+        } else {
+            txtb.value = 1;
+        }
+    }
+}
+
+
+
+const btSummit = document.getElementById('sub'); //.addEventListener("click", process);
+const btplus = document.getElementById('plus_buton');
+const btminus = document.getElementById('minus_buton');
+
+btSummit.addEventListener("click", process);
+btplus.addEventListener("click", add);
+btminus.addEventListener("click", minus);
