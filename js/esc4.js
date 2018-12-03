8a8844c4e8c22fa38c806e3fb9a4ce8721a9b1f8
DEPRATI\spilay <samuel.pilay@deprati.com.ec>
2018-12-03 11:45:04 -0500

    Se agregan los nombres de los plugins a cada nodo interactivo

--- js/esc4.js
+++ js/esc4.js
@@ -2,8 +2,13 @@ function interaccion(area) {
 
     //console.log(area);
     console.log(`area seleccionada: ${area.target.id}`);
+    console.log(`area alt: ${area.target.alt}`);
+    console.log(`plugins: ${area.target.dataset.plugins}`);
     console.log(`plugins: ${area.target.dataset.plugins}`);
 
+
+
+    SeeNodeSelected(area.target.alt);
     setViewPluginsByCaseStudi_cs(area.target.dataset.plugins);
     setViewPluginsByCaseStudi_bor(area.target.dataset.plugins);
     setViewPluginsByCaseStudi_bof(area.target.dataset.plugins);
@@ -14,3 +19,16 @@ const areas = document.querySelectorAll("area");
 areas.forEach(area => {
     area.addEventListener("click", interaccion)
 });
+
+
+
+function SeeNodeSelected(node) {
+    var div = document.getElementById("NodeSelected");
+    if (node && node.length > 0) {
+        div.classList.add("ui", "red", "label")
+        div.innerHTML = node;
+    } else {
+        div.classList.remove("ui", "red", "label")
+        div.innerHTML = "";
+    }
+}
